26ede39624303646274cf234a9fd76ef8f834b34
Jeroen Ooms <jeroenooms@gmail.com>
2016-03-16 17:21:45 +0100

    Backport windows tweaks

--- src/convert.cpp
+++ src/convert.cpp
@@ -1,6 +1,12 @@
 #include <errno.h>
 #include "convert.h"
 
+#ifdef _WIN32
+#define ICONV_CONST_FIX (const char**)
+#else
+#define ICONV_CONST_FIX
+#endif
+
 iconv_t new_iconv(const char * from, const char * to){
   iconv_t cd = iconv_open(to, from);
   if(cd == (iconv_t) -1){
@@ -19,10 +25,10 @@ char * string_from_r(Rcpp::String str, iconv_t cd){
   size_t outlen = 4 * inlen + 1;
   char output[outlen];
   char * cur = output;
-  size_t success = iconv(cd, &inbuf, &inlen, &cur, &outlen);
+  size_t success = iconv(cd, ICONV_CONST_FIX &inbuf, &inlen, &cur, &outlen);
   if(success == (size_t) -1)
     return NULL;
-  cur[0] = '\0';
+  *cur = '\0';
   char * res = (char *) malloc(outlen + 1);
   strcpy(res, output);
   return res;
@@ -33,10 +39,10 @@ Rcpp::String string_to_r(char * inbuf, iconv_t cd){
   size_t outlen = 4 * inlen + 1;
   char output[outlen];
   char * cur = output;
-  size_t success = iconv(cd, &inbuf, &inlen, &cur, &outlen);
+  size_t success = iconv(cd, ICONV_CONST_FIX &inbuf, &inlen, &cur, &outlen);
   if(success == (size_t) -1)
     return NA_STRING;
-  cur[0] = '\0';
+  *cur = '\0';
   Rcpp::String res = Rcpp::String(output);
   res.set_encoding(CE_UTF8);
   return res;
