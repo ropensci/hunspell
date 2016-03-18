641da2aa2f17d6e852435c3243622aa253863533
Jeroen Ooms <jeroenooms@gmail.com>
2016-03-18 17:51:02 +0100

    Doc

--- R/hunspell.R
+++ R/hunspell.R
@@ -1,36 +1,37 @@
 #' Hunspell Spell Checking
 #'
-#' The \code{\link{hunspell_check}} function takes a vector of individual words and tests
-#' each one for correctness. The \code{\link{hunspell_find}} function takes a character
-#' vector with text (plain, latex or man format), parses out the words and returns a
-#' list with incorrect words for each line. Finally \code{\link{hunspell_suggest}}
-#' is used to suggest correct alternatives for each (incorrect) input word.
+#' The \code{\link{hunspell_find}} function takes a character vector with text
+#' (plain, latex or man format), parses out the words and returns a list with
+#' incorrect words for each line. See details for operations on individual words.
 #'
-#' The \code{\link{hunspell_analyze}} function shows how a word breaks down into a
-#' valid stem plus affix. The \code{\link{hunspell_stem}} function only returns valid
-#' stems for a given word. Hunspell uses a special dictionary format that defines
-#' which stems and affixes are valid in a given language. Among other things, stemming
-#' can be used to summarize text (e.g wordcloud).
+#' Hunspell uses a special dictionary format that defines which stems and affixes are
+#' valid in a given language. The \code{\link{hunspell_analyze}} function shows how a
+#' word breaks down into a valid stem plus affix. The \code{\link{hunspell_stem}}
+#' function is similar but only returns valid stems for a given word. Stemming can be
+#' used to summarize text (e.g in a wordcloud). The \code{\link{hunspell_check}} function
+#' takes a vector of individual words and tests each one for correctness. Finally
+#' \code{\link{hunspell_suggest}} is used to suggest correct alternatives for each
+#' (incorrect) input word.
 #'
 #' The package searches for dictionaries in the working directory as well as in the
-#' standard system dictionary locations. Additional search paths can be added by setting
-#' the \code{DICPATH} environment variable. A US-English dictionary is included with
-#' the package; other dictionaries need to be installed by the system. Most operating
-#' systems already include standard dictionaries with names such as
-#' \href{https://packages.debian.org/sid/hunspell-en-gbl}{hunspell-en-gb} or
+#' standard system locations. Additional search paths can be specified by setting
+#' the \code{DICPATH} environment variable. A US English dictionary (\code{en_US}) is
+#' included with the package; other dictionaries need to be installed by the system.
+#' Most operating systems already include compatible dictionaries with names such as
+#' \href{https://packages.debian.org/sid/hunspell-en-gb}{hunspell-en-gb} or
 #' \href{https://packages.debian.org/sid/myspell-en-gb}{myspell-en-gb}.
 #'
 #' To manually install dictionaries, download the \code{.aff} and \code{.dic} file
 #' from an OpenOffice \href{http://ftp.snt.utwente.nl/pub/software/openoffice/contrib/dictionaries/}{mirror}
 #' or \href{http://archive.ubuntu.com/ubuntu/pool/main/libr/libreoffice-dictionaries/?C=S;O=D}{bundle}
 #' and copy them to \code{~/Library/Spelling} or a custom directory specified in \code{DICPATH}.
-#' Alternatively you can pass the entire path to the \code{.aff} and \code{.dic} file
-#' as the \code{dict} parameter.
+#' Alternatively you can pass the entire path to the \code{.dic} file as the \code{dict}
+#' parameter.
 #'
-#' Note that \code{hunspell_find} uses iconv to convert input text to the encoding
-#' used by the dictionary. This will fail if \code{text} contains characters which are
-#' unsupported by that particular encoding. For this reason it is safer to use UTF8
-#' dictionaries which can represent all unicode characters. Several UTF8 dictionaries are
+#' Note that \code{hunspell_find} uses \code{\link{iconv}} to convert input text to
+#' the encoding used by the dictionary. This will fail if \code{text} contains characters
+#' which are unsupported by that particular encoding. For this reason UTF-8 dictionaries
+#' are preferrable over legacy 8bit dictionaires. Several UTF8 dictionaries are
 #' available from \href{https://github.com/titoBouzout/Dictionaries}{Github}.
 #'
 #' @rdname hunspell
