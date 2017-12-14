7d341b31c2e0992deaa7822aa57090a4c31b3f40
Jeroen <jeroenooms@gmail.com>
2017-12-14 15:16:50 +0100

    Support apostrophe in en_US

--- tests/testthat/test_parser.R
+++ tests/testthat/test_parser.R
@@ -9,3 +9,37 @@ test_that("Parser works for large strings",{
     expect_length(hunspell_parse(y, dict = "russian-aot")[[1]], 1300)
   }
 })
+
+# contractions and posessive are the most challenging to check
+test_that("Test English apostrophe",{
+  text <- c(
+    "let's, don't, couldn't, it's, she's",
+    "the lawyer's fee",
+    "the child's toy",
+    "children's toys",
+    "Tom Jones's first album",
+    "anyone's guess"
+  )
+
+  # Do not split at the apostrophe
+  expect_equal(sapply(hunspell_parse(text, dict = 'en_US'), length), c(5, 3, 3, 2, 4, 2))
+  expect_equal(sapply(hunspell_parse(text, dict = 'en_GB'), length), c(5, 3, 3, 2, 4, 2))
+
+  # Do not find any typos
+  lapply(text, function(x){
+    expect_length(hunspell(x, dict = 'en_GB')[[1]], 0)
+    expect_length(hunspell(x, dict = 'en_US')[[1]], 0)
+  })
+
+  # These only work in the GB dictionary it seems:
+  text <- c(
+    "excessive lawyers' fees",
+    "the twins' parents",
+    "the student teachers' supervisor",
+    "the boys' baseball team"
+  )
+  lapply(text, function(x){
+    expect_length(hunspell(x, dict = 'en_GB')[[1]], 0)
+    #expect_length(hunspell(x, dict = 'en_US')[[1]], 0)
+  })
+})
